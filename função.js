a26f2f5e3d94ec12488b23185d575dbb3b400d2d
Gislene Barros <laineb.sousa@gmail.com>
2022-03-07 14:36:06 -0300

    javascript:função

---
+++ função.js
@@ -0,0 +1,13 @@
+let ola = function (){
+    console.log("vou me comportar direitinho");
+};
+
+function sum (x){
+    total = 2 * x + 1
+    return total;
+}
+
+function sum (x, y, z){
+    console.log('x', 'y', 'z')
+    return x * y - y * z;
+}
